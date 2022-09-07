c031c70210eb86388ca4e5c7ae3866bafdcc6e48
Jason <jatsburgern@gmail.com>
2022-09-06 20:14:05 -0400

    added root

---
+++ src/Root.js
@@ -0,0 +1,10 @@
+import App from "./App"
+import { BrowserRouter } from 'react-router-dom';
+function Root() {
+    return (
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    );
+}
+export default Root;
